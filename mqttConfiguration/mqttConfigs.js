e767c490fbd17d294b10e8ac1c988865f678e0e9
guruakaashjn <gurujnn@gmail.com>
2025-03-01 20:15:52 +0530

    add mqtt configurations

---
+++ mqttConfiguration/mqttConfigs.js
@@ -0,0 +1,7 @@
+// MQTT Broker configurations
+const BROKER_URL = 'mqtt://mqtt.eclipseprojects.io';
+const SENSOR_TOPIC = 'iot/airpurifier/sensors';
+const FAN_SPEED_TOPIC = 'iot/airpurifier/fan/set';
+const FAN_SPEED_ACK_TOPIC = 'iot/airpurifier/fan/ack';
+
+export { BROKER_URL, SENSOR_TOPIC, FAN_SPEED_TOPIC, FAN_SPEED_ACK_TOPIC };
